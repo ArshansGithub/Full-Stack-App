e65655dae2ab6b61f3658da3e62da5bc6fefe447
Arshan <arshan@arshan.dev>
2024-06-21 20:21:45 -0700

    database stuff + login page changes

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
 import pymongo
 from pymongo import MongoClient
+import dotenv, os
 
-secret_key = "asdsagasdsadsadsadghasdsad3"
+dotenv.load_dotenv()
+
+secret_key = os.environ.get("SECRET_KEY")
 
 client = MongoClient('localhost', 27017)
 db = client['chipotleapi']
--- static/js/main.js
+++ static/js/main.js
@@ -1,55 +1,72 @@
-url = "http://localhost:5000"
-function register() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+const url = "http://localhost:5000";
 
-    var data = {
+async function register() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const data = {
         username: username,
-        password: password,
-        type: "password"
+        password: password
     };
 
-    const req = new XMLHttpRequest();
-    req.open("POST", url + "/api/v1/register");
-    req.setRequestHeader("Content-Type", "application/json");
-    req.dataType = "json";
-    req.onload = function() {
-        console.log()
-        if (req.status == 200) {
-            alert("Success");
-        } else if (req.getResponseHeader("content-type") == "application/json") {
-            alert(JSON.parse(req.response).message);
-        } else {
-            alert("Something went wrong! Error code: " + req.status);
-        }
-    };
-    req.send(JSON.stringify(data));
+    try {
+        const response = await fetch(url + "/api/v1/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+
+        await handleResponse(response, {
+            200: () => {
+                alert("Success");
+            }
+        });
+    } catch (error) {
+        alert("Something went wrong! Error: " + error.message);
+    }
 }
 
-function login() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+async function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
 
-    var data = {
+    const data = {
         username: username,
-        password: password,
-        type: "password"
+        password: password
     };
 
-    const req = new XMLHttpRequest();
-    req.open("POST", url + "/api/v1/login");
-    req.setRequestHeader("Content-Type", "application/json");
-    req.dataType = "json";
-    req.withCredentials = true;
-    req.onload = function() {
-        if (req.status == 200) {
-            window.location.href = req.responseURL;
-        } else if (req.getResponseHeader("content-type") == "application/json") {
-            alert(JSON.parse(req.response).message);
-        } else {
-            alert("Something went wrong! Error code: " + req.status);
-        }
-    };
-    req.send(JSON.stringify(data));
+    try {
+        const response = await fetch(url + "/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(data)
+        });
+
+        await handleResponse(response, {
+            200: () => {
+                window.location.href = response.url;
+            }
+        });
+    } catch (error) {
+        alert("Something went wrong! Error: " + error.message);
+    }
 }
 
+async function handleResponse(response, handlers) {
+    const contentType = response.headers.get("content-type");
+
+    if (handlers[response.status]) {
+        const jsonResponse = contentType && contentType.includes("application/json") ? await response.json() : null;
+        handlers[response.status](jsonResponse);
+    } else if (contentType && contentType.includes("application/json")) {
+        const jsonResponse = await response.json();
+        alert(jsonResponse.message || "Something went wrong! Error code: " + response.status);
+    } else {
+        alert("Something went wrong! Error code: " + response.status);
+    }
+}
